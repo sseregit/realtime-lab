8d670ad595dd6f132f7cbfc7ea335dd652e381b8
장연식 <sseregit@knowwheresoft.com>
2025-07-30 16:43:04 +0900

    LongPolling blocking 요청대기 상태 작성

--- src/main/java/io/github/sseregit/realtimelab/longpolling/LongPollingController.java
+++ src/main/java/io/github/sseregit/realtimelab/longpolling/LongPollingController.java
@@ -1,14 +1,32 @@
 package io.github.sseregit.realtimelab.longpolling;
 
-import org.springframework.http.ResponseEntity;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 class LongPollingController {
 
+    private final Map<String, DeferredResult<String>> waitingUsers = new ConcurrentHashMap<>();
+
     @GetMapping("/longpolling")
-    ResponseEntity<String> longPolling() {
-        return ResponseEntity.ok("LongPolling");
+    DeferredResult<String> longPolling(@RequestParam String userId) {
+        log.info("클라이언트 [{}] 대기 시작", userId);
+        DeferredResult<String> result = new DeferredResult<>(5000L, "no message");
+
+        result.onCompletion(() -> {
+            log.info("클라이언트 [{}] 응답 완료 또는 타임아웃", userId);
+            waitingUsers.remove(userId);
+        });
+
+        waitingUsers.put(userId, result);
+        return result;
     }
 }
